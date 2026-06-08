8d18bad4cf12c6b970b3bdc446cf0b9a219a32e3
Muhammad <botskomilov@gmail.com>
2026-06-08 12:26:41 +0500

    style: configure Google Sans font family globally

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -5,6 +5,9 @@ export default function Document() {
     <Html lang="uz">
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Google+Sans:ital,opsz,wght@0,17..18,400..700;1,17..18,400..700&display=swap" rel="stylesheet" />
       </Head>
       <body className="min-h-full flex flex-col antialiased">
         <Main />
